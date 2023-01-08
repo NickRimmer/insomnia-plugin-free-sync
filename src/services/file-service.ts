ceb5a45c73b80c1da1f93b074bec67cd51b15cee
Viacheslav Lepshin <viacheslav.lepshin@accelleron-industries.com>
2023-01-08 22:49:57 +0100

    configuration dialog refactored

---
+++ src/services/file-service.ts
@@ -0,0 +1,7 @@
+﻿export const validatePath = (path: string | null | undefined): boolean =>
+  path !== null &&
+  path !== undefined &&
+  path.length > 0 &&
+  new RegExp(
+    '^(?:[a-z]:)?[\\/\\\\]{0,2}(?:[.\\/\\\\ ](?![.\\/\\\\\\n])|[^<>:"|?*.\\/\\\\ \\n])+$', 'gmi')
+    .exec(path) !== null
