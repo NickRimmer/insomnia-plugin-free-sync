4e502e3f46e1dfa79c6f2dac80a550121e95659e
Viacheslav Lepshin <viacheslav.lepshin@accelleron-industries.com>
2023-01-08 20:15:21 +0100

    minor changes

--- src/insomnia/tools/dialogs.ts
+++ src/insomnia/tools/dialogs.ts
@@ -3,18 +3,19 @@ import ReactDOM from 'react-dom'
 import { ReactElement } from 'react'
 
 export type showDialogComponentProps = {
+  title: string,
   context: InsomniaContext,
   children: ReactElement<any, any>
 }
 
-export const showDialogComponent = ({context, children}: showDialogComponentProps): void => {
+export const showDialogComponent = ({context, title, children}: showDialogComponentProps): void => {
   const root: HTMLElement = document.createElement('div')
   ReactDOM.render(
     children,
     root,
   )
 
-  context.app.dialog('Free sync: Settings', root, {
+  context.app.dialog(title, root, {
     skinny: true,
     onHide() {
       ReactDOM.unmountComponentAtNode(root)
--- src/workspace-actions.tsx
+++ src/workspace-actions.tsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React from 'react'
 import { InsomniaWorkspaceAction } from './insomnia/types/workspace-action.types'
 import { showDialogComponent } from './insomnia/tools/dialogs'
 import { ConfigurationDialog } from './components/configuration-dialog'
 
-const forceRunAction: InsomniaWorkspaceAction = {
-  label: 'Free sync: Force run',
-  icon: 'fa-arrows-rotate',
-  action: () => alert('Not implemented yet')
+const saveAction: InsomniaWorkspaceAction = {
+  label: 'Free sync: Save workspace',
+  icon: 'fa-upload',
+  action: () => alert('Not implemented yet'),
+}
+
+const readAction: InsomniaWorkspaceAction = {
+  label: 'Free sync: Load workspace',
+  icon: 'fa-download',
+  action: () => alert('Not implemented yet'),
 }
 
 const configurationAction: InsomniaWorkspaceAction = {
@@ -14,11 +20,13 @@ const configurationAction: InsomniaWorkspaceAction = {
   icon: 'fa-cog',
   action: context => showDialogComponent({
     context,
-    children: (<ConfigurationDialog context={context}/>)
-  })
+    title: 'Free sync: Configuration',
+    children: (<ConfigurationDialog context={context}/>),
+  }),
 }
 
 export const workspaceActions: InsomniaWorkspaceAction[] = [
-  forceRunAction,
-  configurationAction
-];
+  saveAction,
+  readAction,
+  configurationAction,
+]
